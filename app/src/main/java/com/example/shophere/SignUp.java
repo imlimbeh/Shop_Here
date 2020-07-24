e3b82c2f682b39886b5a20d40e150032a02b704a
Lein Yi Shun <eason7716@gmail.com>
2020-07-24 11:05:52 +0800

    commit by Lein Yi Shun

---
+++ app/src/main/java/com/example/shophere/SignUp.java
@@ -0,0 +1,14 @@
+package com.example.shophere;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
+}
