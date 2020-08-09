8b7427bf7557d581eacf3fa6c12c2d98ae0d58b7
liewmh0077 <liewmh-wm19@student.tarc.edu.my>
2020-08-09 15:57:54 +0800

    commit by minhui

---
+++ app/src/main/java/com/example/shophere/payment_detail.java
@@ -0,0 +1,14 @@
+package com.example.shophere;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class payment_detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_detail);
+    }
+}
